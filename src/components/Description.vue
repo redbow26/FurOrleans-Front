c5e9aa2e517cac7f604d840c07fc513d50315240
Redbow <tristanleroy26@gmail.com>
2021-02-12 13:46:08 +0100

    feat: Description component

---
+++ src/components/Description.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="description">
+    <h1 class="name">{{ name }}</h1>
+    <p class="">{{ description }}</p>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Description",
+  props: {
+    description: {
+      type: String,
+      default: ""
+    },
+    name: {
+      type: String,
+      default: "FurOrléans"
+    }
+  }
+});
+</script>
+
+<style scoped lang="scss" />
