0ab3e0cc91a1b93565a9a680a946395d3f248bc5
Dreichi <damien.jenger@gmail.com>
2021-02-12 20:47:53 +0100

    feat: Article component

---
+++ src/components/Article.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="article">
+        <img class="thumbnail" :src="imageUrl" alt="">
+        <div class="text">
+        <h3 class="title">{{ title }}</h3>
+        <p class="description">{{ description }}</p>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import {defineComponent} from "vue";
+
+export default defineComponent({
+    name: "article",
+    props: {
+        title: {
+            type: String,
+        },
+        imageUrl: {
+            type: String,
+        },
+        description: {
+            type: String,
+        },
+    }
+});
+</script>
+
+<style scoped lang="scss" src="../assets/styles/Article.scss" />
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,24 @@
 <template>
   <div class="home">
-    <Description description="Little description"></Description>
+    <Article imageUrl="https://zupimages.net/up/21/06/szk1.png"
+      title="furorléans" 
+      description="Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500." 
+    />
+    <Article imageUrl="https://zupimages.net/up/21/06/szk1.png"
+      title="furorléans" 
+      description="Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500." 
+    />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import Description from "@/components/Description.vue"; // @ is an alias to /src
+import Article from "@/components/Article.vue";
 
 export default defineComponent({
   name: "Home",
   components: {
-    Description
+    Article
   }
 });
 </script>
